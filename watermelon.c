71f20978adab5830fdfa5734981b43cf31b0e571
Ahmad Fahmid <112432485+alvi00@users.noreply.github.com>
2022-10-31 13:19:58 +0600

    Update watermelon.c

--- watermelon.c
+++ watermelon.c
@@ -1,28 +1,17 @@
+//submitted in codeforces and been accepted
 #include<stdio.h>
 int main()
 {
     int a;
-    printf("Enter the weight\n");
     scanf("%d",&a);
-    if(a>=0 && a<=100)
+    if(a%2==0 && a>2)
     {
-        if(a==2)
-        {
-            printf("NO\n");
-        }
-        else if(a%2==0)
-        {
             printf("YES\n");
-        }
-        else
-        {
-            printf("NO\n");
-        }
-
     }
+
     else
     {
-        printf("Enter number between o and 100");
+        printf("NO");
     }
     return 0;
 }
